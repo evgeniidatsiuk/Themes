77b414ca1ba0aa1f786a733716c8f9f57666c523
evgeniidatsiuk <evgeniidatsiuk@gmail.com>
2019-02-19 23:33:48 +0200

    avatar for userparam & using gem paperclip

--- app/models/userparam.rb
+++ app/models/userparam.rb
@@ -1,3 +1,6 @@
 class Userparam < ApplicationRecord
   belongs_to :user
+
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "50x50" }, default_url: "/assets/missing.png"
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_19_151551) do
+ActiveRecord::Schema.define(version: 2019_02_19_212438) do
 
   create_table "userparams", force: :cascade do |t|
     t.integer "user_id"
@@ -20,6 +20,10 @@ ActiveRecord::Schema.define(version: 2019_02_19_151551) do
     t.integer "age"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "avatar_file_name"
+    t.string "avatar_content_type"
+    t.integer "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
