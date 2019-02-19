b9e5bae5bb5e1694bfa5f1fdfb909cc71ce92690
evgeniidatsiuk <evgeniidatsiuk@gmail.com>
2019-02-19 19:35:12 +0200

    update userparams

--- app/controllers/userparams_controller.rb
+++ app/controllers/userparams_controller.rb
@@ -1,10 +1,38 @@
 class UserparamsController < ApplicationController
-  def new
-  end
+  before_action :authenticate_user!
 
-  def show
-  end
+    def new
+      @userparam = Userparam.new
+    end
 
-  def edit
+    def create
+      @userparam = current_user.build_userparam(profile_params)
+      if @userparam.save
+        redirect_to userparam_path(current_user.id)
+      else render 'new'
+      end
+
+    end
+
+    def show
+    end
+
+    def edit
+      @userparam = current_user.userparam
+    end
+
+    def update
+      @userparam = Userparam.find_by(user_id: current_user.id)
+      if @userparam.update(profile_params)
+         redirect_to userparam_path(current_user.id)
+      else render 'edit'
+      end
+
+    end
+
+  private
+
+    def profile_params
+      params.require(:userparam).permit(:user_id, :nickname, :firstname, :lastname, :age)
   end
 end
--- app/models/userparam.rb
+++ app/models/userparam.rb
@@ -1,2 +1,3 @@
 class Userparam < ApplicationRecord
+  belongs_to :user
 end
