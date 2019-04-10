037836c170cc45d7d735971a934a801282b7e643
E D <evgenii.datsiuk@gmail.com>
2019-04-11 01:44:00 +0300

    update views field

--- app/controllers/load/windows_seven_themes_controller.rb
+++ app/controllers/load/windows_seven_themes_controller.rb
@@ -1,9 +1,17 @@
 class Load::WindowsSevenThemesController < ApplicationController
+  before_action :find
   def index
     @windows_seven_themes = WindowsSevenTheme.all
   end
 
   def show
+    @windows_seven_theme.views +=1
+    @windows_seven_theme.save
+    end
+
+  private
+  
+  def find
     @windows_seven_theme = WindowsSevenTheme.find(params[:id])
   end
 end
