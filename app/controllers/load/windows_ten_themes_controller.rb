6df1b26016125dc88fd39f62db1169530dd69821
evgeniidatsiuk <evgenii.datsiuk@gmail.com>
2019-04-21 12:47:41 +0300

    update controller windows_ten

--- app/controllers/load/windows_ten_themes_controller.rb
+++ app/controllers/load/windows_ten_themes_controller.rb
@@ -1,7 +1,17 @@
 class Load::WindowsTenThemesController < ApplicationController
+  before_action :find
   def index
+    @windows_ten_themes = WindowsTenTheme.all
   end
 
   def show
+    @windows_ten_theme.views +=1
+    @windows_ten_theme.save
+  end
+
+  private
+
+  def find
+    @windows_ten_theme = WindowsTenTheme.find(params[:id])
   end
 end
