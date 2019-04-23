48c661bb230287441c353b9a8c80ff3eda29921a
evgeniidatsiuk <evgenii.datsiuk@gmail.com>
2019-04-24 00:52:37 +0300

    Redirect back to current page after sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :store_user_location!, if: :storable_location?
   protect_from_forgery with: :exception
   protected
 
@@ -8,4 +9,15 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_in, keys: %i[login password password_confirmation])
     devise_parameter_sanitizer.permit(:account_update, keys: %i[nickname email password password_confirmation current_password])
   end
+
+  private
+
+   def storable_location?
+      request.get? && is_navigational_format? && !devise_controller? && !request.xhr?
+    end
+
+    def store_user_location!
+      # :user is the scope we are authenticating
+      store_location_for(:user, request.fullpath)
+    end
 end
